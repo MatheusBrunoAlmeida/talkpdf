122b541dab1998e0c15d546df396b85a9c690755
MatheusBrunoAlmeida <manancialsup@hotmail.com>
2024-08-27 18:46:15 -0300

    stay traing

--- src/app/api/webhooks/stripe/route.ts
+++ src/app/api/webhooks/stripe/route.ts
@@ -1,43 +1,43 @@
-import { db } from '@/db'
-import { stripe } from '@/lib/stripe'
-import { headers } from 'next/headers'
-import type Stripe from 'stripe'
-import { buffer } from 'micro'
+import { db } from '@/db';
+import { stripe } from '@/lib/stripe';
+import { headers } from 'next/headers';
+import { NextResponse } from 'next/server';
+import type Stripe from 'stripe';
+import { buffer } from 'micro';
 
 export async function POST(request: Request) {
-  const signature = request.headers.get('stripe-signature') ?? ''
-  // @ts-ignore
-  const rawBody = await buffer(request.body);
+  const signature = headers().get('stripe-signature') ?? '';
+  const rawBody = await request.arrayBuffer();
 
   if (!rawBody) {
     throw new Error('Body is empty or not raw');
   }
 
-  let event: Stripe.Event
+  let event: Stripe.Event;
 
   try {
     // Verifica a assinatura do webhook com o corpo cru
     event = await stripe.webhooks.constructEventAsync(
-      rawBody,
+      Buffer.from(rawBody),
       signature,
       process.env.STRIPE_WEBHOOK_SECRET || ''
-    )
+    );
 
     // Processa o evento do Stripe
-    const session = event.data.object as Stripe.Checkout.Session
+    const session = event.data.object as Stripe.Checkout.Session;
 
-    console.log('session', session)
+    console.log('session', session);
 
     if (!session?.metadata?.userId) {
-      return new Response(null, { status: 200 })
+      return NextResponse.json({}, { status: 200 });
     }
 
     if (event.type === 'checkout.session.completed') {
       const subscription = await stripe.subscriptions.retrieve(
         session.subscription as string
-      )
+      );
 
-      console.log('webhook called')
+      console.log('webhook called');
 
       await db.user.update({
         where: { id: session.metadata.userId },
@@ -49,13 +49,13 @@ export async function POST(request: Request) {
             subscription.current_period_end * 1000
           ),
         },
-      })
+      });
     }
 
     if (event.type === 'invoice.payment_succeeded') {
       const subscription = await stripe.subscriptions.retrieve(
         session.subscription as string
-      )
+      );
 
       await db.user.update({
         where: { stripeSubscriptionId: subscription.id },
@@ -65,17 +65,16 @@ export async function POST(request: Request) {
             subscription.current_period_end * 1000
           ),
         },
-      })
+      });
     }
 
-    return new Response(null, { status: 200 })
+    return NextResponse.json({}, { status: 200 });
   } catch (err) {
     // Captura qualquer erro relacionado à verificação da assinatura ou ao processamento do evento
-    console.error(`Webhook Error: ${err instanceof Error ? err.message : 'Unknown Error'}`)
-    return new Response(
-      `Webhook Error: ${err instanceof Error ? err.message : 'Unknown Error'
-      }`,
+    console.error(`Webhook Error: ${err instanceof Error ? err.message : 'Unknown Error'}`);
+    return NextResponse.json(
+      { error: `Webhook Error: ${err instanceof Error ? err.message : 'Unknown Error'}` },
       { status: 400 }
-    )
+    );
   }
 }
